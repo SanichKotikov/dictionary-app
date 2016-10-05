c101932d2bc7761694561252615133a185d256a1
Sanich <sancho@cdrpro.ru>
2016-10-05 18:05:54 +0300

    Fixed spelling

--- app/components/favorite-card.js
+++ app/components/favorite-card.js
@@ -8,15 +8,15 @@ class FavoriteCard {
     }
 
     html() {
-        const dist = this._dict;
+        const dict = this._dict;
 
         const html = document.importNode(this._cardTpl.content, true);
         const divs = html.querySelectorAll('.favorite-card > div');
 
-        html.querySelector('.favorite-card').dataset.name = dist.text;
+        html.querySelector('.favorite-card').dataset.name = dict.text;
 
-        divs[0].innerHTML = `${dist.text} <span class="transcription">[${dist.data[0].ts}]<span>`;
-        divs[1].textContent = dist.data.map(item => item.tr[0].text).join(', ');
+        divs[0].innerHTML = `${dict.text} <span class="transcription">[${dict.data[0].ts}]<span>`;
+        divs[1].textContent = dict.data.map(item => item.tr[0].text).join(', ');
 
         return html;
     }
